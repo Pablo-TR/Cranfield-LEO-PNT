f857f76c4f0a2a8cbfd54cd47d73f3ac5c921b40
alfredo-lavin <alfredo.lavin@hotmail.com>
2025-11-07 14:14:43 +0000

    Added coding template
    It should be included in every piece of code developed by the team.

---
+++ Algorithm/codingTemplate.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%                            ${fileName}.m
+%
+% Copyright: Cranfield University
+% Project Name: LEO-PNT
+% Module: [Module Name, e.g. Tracking]
+% Author: [Your Name]
+% Date: ${date}
+% Last Update: [Last Update Date]
+% Version: 1.0.0
+%
+% Description: [A brief description of the file's purpose and contents]
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc; clear; close all;  % Reset MATLAB environment
+
+
