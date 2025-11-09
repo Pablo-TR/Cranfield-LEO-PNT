30a915e3c4cdf49506dbd3b07b84284298dbd6eb
alfredo-lavin <alfredo.lavin@hotmail.com>
2025-11-09 23:10:56 +0000

    Add header comments to plotFFT.m
    Added header comments with project details and description.

--- Algorithm/Plots/plotFFT.m
+++ Algorithm/Plots/plotFFT.m
@@ -1,3 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%                            plotFFT.m
+%
+% Copyright: Cranfield University
+% Project Name: LEO-PNT
+% Module: Plots
+% Author: Pablo, Marti, Dan and Fredo
+% Date: 9th November 2025
+% Last Update: 9th November 2025
+% Version: 1.0.0
+%
+% Description: Plots the Fast Fourier Transform of the signal in dB.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function plotFFT(fShifted, signalFFT)
     figure(1);
     plot(fShifted, 10*log10(abs(fftshift(signalFFT))));
