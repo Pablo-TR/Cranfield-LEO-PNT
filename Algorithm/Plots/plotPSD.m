582d071183a2c55eb755a19f6d85dd23f4a20e41
alfredo-lavin <alfredo.lavin@hotmail.com>
2025-11-09 23:11:52 +0000

    Add header to plotPSD.m
    Added header comments and description for plotPSD function.

--- Algorithm/Plots/plotPSD.m
+++ Algorithm/Plots/plotPSD.m
@@ -1,3 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%                            plotPSD.m
+%
+% Copyright: Cranfield University
+% Project Name: LEO-PNT
+% Module: Plots
+% Author: Pablo, Marti, Dan and Fredo
+% Date: 9th November 2025
+% Last Update: 9th November 2025
+% Version: 1.0.0
+%
+% Description: Plots the Power Spectral Density of the signal in dB.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function plotPSD(fpsd, pxx)
     plot(fpsd, 10*log10(pxx)); 
     title('"Smooth" PSD (pwelch with averaging)');
